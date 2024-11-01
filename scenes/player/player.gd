8a6daa00131a7182fe774ec425736b527e3f780a
Eric-Chrisman <ericchrisman01@gmail.com>
2024-11-01 11:39:33 -0500

    a

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,21 +1,38 @@
 extends CharacterBody2D
 
 @export var aniSprite:AnimatedSprite2D = null
+@export var hurtSoundEffect:AudioStreamPlayer2D = null
+@export var horraySoundEffect:AudioStreamPlayer2D = null
+@export var jumpSoundEffect:AudioStreamPlayer2D = null
+@export var myPosition:CollisionShape2D = null
 
 var gravity:float = 980
 var moveSpeed:float = 500
 var jumpPower:float = -400
+var maxJumpCount:int = 2
+var jumpCount = maxJumpCount
 
 signal playerHit
 signal playerGoal
 
+func _ready():
+	if jumpSoundEffect != null:
+		jumpSoundEffect.max_polyphony = jumpCount
+
 func _process(delta):
 	if not is_on_floor():
+		if jumpCount == maxJumpCount:
+			jumpCount = maxJumpCount - 1
 		velocity.y  += gravity * delta
-	elif Input.is_action_just_pressed("space"):
-		velocity.y = jumpPower
-
+	else:
+		jumpCount = maxJumpCount
 	
+	if Input.is_action_just_pressed("space") and jumpCount > 0:
+		velocity.y = jumpPower
+		jumpCount -= 1;
+		if jumpSoundEffect != null:
+			jumpSoundEffect.play()
+			
 	var moveVector:float = 0
 	if Input.is_action_pressed("left"):
 		moveVector -= moveSpeed
@@ -36,6 +53,11 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	if body.ID == "hurt":
+		if hurtSoundEffect != null:
+			hurtSoundEffect.play()
 		emit_signal("playerHit")
+		
 	elif body.ID == "flag":
+		if horraySoundEffect != null:
+			horraySoundEffect.play()
 		emit_signal("playerGoal")
